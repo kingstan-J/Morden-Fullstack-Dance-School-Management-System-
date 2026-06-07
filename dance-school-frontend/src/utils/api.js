b53165d6c693d23dd42287f3c3ba470fea3ea2d6
Kingstan <kingstan22082007@gmail.com>
2026-06-07 22:32:17 +0530

    last changes

--- dance-school-frontend/src/utils/api.js
+++ dance-school-frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+export const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000';
 const api = axios.create({ baseURL: '/api' });
 
 api.interceptors.request.use((config) => {
